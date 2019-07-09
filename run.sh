6738044e42af992ee1b267ee086108bf5e418de6
Mohammadreza Noormandipour <noormanm@olsky07.cern.ch>
2019-07-09 10:56:17 +0200

    N=2 case Implementation.

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ mpiicpc -g -O3 -DUSE_MM_MALLOC -DSTANDALONE -DINTELQS_HAS_MPI -DMKL_ILP64 -Wall 
 
 mpiicpc -o test.exe -g -O3 -DUSE_MM_MALLOC -DSTANDALONE -DINTELQS_HAS_MPI -DMKL_ILP64 -Wall -std=c++11 -qopenmp   test.o ../../../qureg/qHiPSTER.a -L/cvmfs/projects.cern.ch/intelsw/psxe/linux/x86_64/2018/compilers_and_libraries_2018.3.222/linux/mkl/lib/intel64 -lmkl_cdft_core -lmkl_intel_ilp64 -lmkl_core -lmkl_intel_thread -lmkl_blacs_intelmpi_ilp64 -lpthread
 
-#./test.exe 12 
+#mpirun -np 94 numactl -m 0 ./test.exe 34 
+./test.exe
